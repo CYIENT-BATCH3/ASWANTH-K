6d0874400d169d36d676b5a3a4e2feff5d9dbe22
ASWANTH70047 <111974656+ASWANTH70047@users.noreply.github.com>
2022-09-05 11:40:02 +0530

    Update WORD_COUNT.C

--- WEEK3/day1/WORD_COUNT.C
+++ WEEK3/day1/WORD_COUNT.C
@@ -7,25 +7,27 @@
 */
 
 
+
 #include <stdio.h>
 
 int main() {
    
 char str[56];//declaring a string
-
-int i=0,count=1;//declaring variables
-printf("enter a string");
+char prev=' ';//declare prev as space
+int i=0,count=0;//declaring variables
+printf("enter a string\n");
 scanf("%[^\n]s",str);//reading str
 while(str[i] !='\0')
 {
-    if(str[i] == ' '||str[i]=='\t')
+    if(str[i] ==' '&&str[i]!=prev)//checking if the character is space or previous
     {
         
-        count++;
+        count++;//incrementing count
         
     }
-   
-    i++;
+   prev=str[i];//changing the previous value as str[i]
+    i++;//incrementing the i value
 }
+count++;//incrementing count
 printf("word count === %d",count);
 }
