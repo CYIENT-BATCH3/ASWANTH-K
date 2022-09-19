c147f120dba13653709ea008885bafb067263dd9
ASWANTH70047 <111974656+ASWANTH70047@users.noreply.github.com>
2022-09-19 22:04:41 +0530

    Create program1.cpp

---
+++ week_5/day1/program1.cpp
@@ -0,0 +1,14 @@
+//program foir reading a number and  print the output 
+
+
+
+#include<iostream>//including header file
+using namespace std;//including std library
+int main()
+{
+	int a;//declaring variable
+	setbuf(stdout,NULL);// printning the output in console
+	cout<<"enter a number"<<endl;
+        cin>>a;//reading input
+        cout<<"a =="<<a;//printing output
+}
